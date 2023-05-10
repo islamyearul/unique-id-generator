fe1f0871c49f774ad826163a8f0e712e6940b1f0
islamyearul <yearulislamonem@gmail.com>
2023-05-10 18:31:07 +0600

    new issue fixed

--- src/LaraUniqueId.php
+++ src/LaraUniqueId.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Str;
 
 class LaraUniqueId
 {
-    public function generate($input, $start_from = null, $pad_len = null, $pad_string = null, $prefix = null, $Year = null)
+    public function generate($input,  $pad_len = null, $pad_string = null, $prefix = null, $year = null)
     {
         $prefix = empty($prefix) ? config('lara-unique-id.prefix') : $prefix;
         $prefix = Str::upper($prefix);
@@ -19,20 +19,19 @@ class LaraUniqueId
             $year = '';
         }
 
-        $start_from = empty($start_from) ? config('lara-unique-id.start_from') : $start_from;
-        $start_from = intval($start_from);
         $input = intval($input); 
         $input = $input + $start_from;
         $pad_direction =config('lara-unique-id.pad_direction');
+        $pad_string = empty($pad_string) ? config('lara-unique-id.pad_string') : $pad_string;
 
         if ($pad_len <= strlen($input)) {
             trigger_error('<strong>$pad_len</strong> cannot be less than or equal to the length of <strong>$input</strong> to generate invoice number', E_USER_ERROR);
         }
 
         if (is_string($prefix)) {
-            return sprintf("%s%s%s%s%s", $prefix,$GetYear,$shop_id, '-', str_pad($input, $pad_len, "0",  $pad_direction));
+            return sprintf("%s%s%s%s", $prefix,$year, '-', str_pad($input, $pad_len, $pad_string,  $pad_direction));
         }
 
-        return sprintf("%s%s%s%s%s", $GetYear,$shop_id, '-', str_pad($input, $pad_len, "0",  $pad_direction));
+        return sprintf("%s%s%s%s", $year, '-', str_pad($input, $pad_len, $pad_string,  $pad_direction));
     }
 }
